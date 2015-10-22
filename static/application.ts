d8b28820a782c693c799c6d3cc18f18c2bdfc9c5
Elias Faxö <elias.faxo@gmail.com>
2015-10-22 19:50:11 +0200

    Stubs.

--- static/application.ts
+++ static/application.ts
@@ -3,5 +3,5 @@ import Test from "./services/test";
 console.log("HEJ");
 var div = document.createElement("div");
 document.body.appendChild(div);
-div.textContent = new Test().getTest() + " HEJ";
+div.textContent = new Test().getTest() + " HEJ!!!";
 //alert(new Test().getTest());
